253cbb4dc1ad2a0202f97c2252dc8cd185d1373f
Amine Elouattar <elouattar@gmail.com>
2018-07-29 22:58:36 +0100

    create album list mvp's contract.

--- app/src/main/java/com/example/amineelouattar/codingchallenge/album_list/AlbumListContract.java
+++ app/src/main/java/com/example/amineelouattar/codingchallenge/album_list/AlbumListContract.java
@@ -1,16 +1,21 @@
 package com.example.amineelouattar.codingchallenge.album_list;
 
 import com.example.amineelouattar.codingchallenge.album_list.model.Album;
+import com.example.amineelouattar.codingchallenge.album_list.model.User;
+import com.facebook.GraphResponse;
 
 import java.util.List;
 
 public interface AlbumListContract {
     interface AlbumListPresenter{
-
+        void getUserInfo();
+        void getAlbums();
+        void extractAlbums(GraphResponse response);
+        void extractUserInfo(GraphResponse response);
     }
 
     interface AlbumListView{
         void updateAlbumList(List<Album> albumList);
-        void updateUserSection();
+        void updateUserSection(User user);
     }
 }
