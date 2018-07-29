7b8f0bca900c97e97298412946c890df6406b0fc
Amine Elouattar <elouattar@gmail.com>
2018-07-30 00:03:12 +0100

    add model method to execute graph path request.

--- app/src/main/java/com/example/amineelouattar/codingchallenge/album_list/AlbumListModel.java
+++ app/src/main/java/com/example/amineelouattar/codingchallenge/album_list/AlbumListModel.java
@@ -6,6 +6,9 @@ import com.facebook.GraphRequest;
 public class AlbumListModel implements AlbumListContract.AlbumListModel {
     @Override
     public void executeGraphRequest(AccessToken accessToken, String graphPath, GraphRequest.Callback callback) {
-
+        GraphRequest request = GraphRequest.newGraphPathRequest(accessToken,
+                graphPath,
+                callback);
+        request.executeAsync();
     }
 }
