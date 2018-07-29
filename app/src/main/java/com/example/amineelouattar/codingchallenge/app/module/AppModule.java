143df6fdf06641ee73887f341a139a1fd4d619a8
Amine Elouattar <elouattar@gmail.com>
2018-07-29 20:40:12 +0100

    Add sharedpreferences method helper in AppModule.

--- app/src/main/java/com/example/amineelouattar/codingchallenge/app/module/AppModule.java
+++ app/src/main/java/com/example/amineelouattar/codingchallenge/app/module/AppModule.java
@@ -1,6 +1,9 @@
 package com.example.amineelouattar.codingchallenge.app.module;
 
 import android.app.Application;
+import android.content.SharedPreferences;
+
+import com.example.amineelouattar.codingchallenge.utils.Variables;
 
 import javax.inject.Singleton;
 
@@ -19,5 +22,11 @@ public class AppModule {
     @Provides
     public Application provideApplication(){
         return application;
+    }
+
+    @Singleton
+    @Provides
+    public SharedPreferences provideSharedPreferences(){
+        return application.getSharedPreferences(Variables.SHARED_PREFERENCES, Variables.SHPREF_MODE);
     }
 }
